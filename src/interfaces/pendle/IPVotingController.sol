bdc82913deb638be1b3b50f5e81361a82574bac3
MrDeadCe11 <mister.deadce11@gmail.com>
2025-09-14 12:30:55 -0500

    removed gov only functions from voting controller interface

---
+++ src/interfaces/pendle/IPVotingController.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Acknowledgment: Interface derived from Pendle V2 (pendle-core-v2-public).
+// Source: https://github.com/pendle-finance/pendle-core-v2-public
+pragma solidity ^0.8.26;
+
+interface IPVotingController {
+    function vote(address[] calldata pools, uint64[] calldata weights) external;
+    function applyPoolSlopeChanges(address pool) external;
+    function finalizeEpoch() external;
+    function broadcastResults(uint64 chainId) external payable;
+}
