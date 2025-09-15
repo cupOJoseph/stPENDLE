14494a8dc0dcec609b2a103bb5f79087926aa807
MrDeadCe11 <mister.deadce11@gmail.com>
2025-09-14 23:44:55 -0500

    added missing files

---
+++ script/DeploystPendle.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+// import {stPENDLE} from "src/stPENDLE.sol";
+
+contract stPendleDeploy is Script {
+    function run() public {
+        /*
+        vm.createSelectFork("https://rpc.ankr.com/eth");
+
+        address merkleDistributor = 0x0000000000000000000000000000000000000000;
+        address vePENDLE = 0x4f30A9D41B80ecC5B94306AB4364951AE3170210;
+        address votingController = 0x0000000000000000000000000000000000000000;
+        address vault = 0x0000000000000000000000000000000000000000;
+        address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT on Ethereum mainnet
+
+        // new stPENDLE(merkleDistributor, vePENDLE, votingController, usdt);
+
+        vm.startBroadcast();
+        vm.stopBroadcast();
+        */
+    }
+}
---
+++ src/dependencies/VaultStructs.sol
@@ -0,0 +1,29 @@
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.0;
+
+    // Withdrawal queue management
+    struct WithdrawalRequest {
+        address receiver;
+        uint256 amount;
+        uint256 requestTime;
+        uint256 eligibleEpoch;
+        bool isProcessed;
+    }
+    
+    struct UserPosition {
+        uint256 totalAmount;
+        uint256 totalLockedPendle;
+        uint256 lastDepositTime;
+    }
+
+    struct VaultPosition {
+        uint256 totalPendleUnderManagement;
+        uint256 totalLockedPendle;
+        uint256 currentEpoch;
+        uint256 epochDuration;
+        uint256 lastEpochUpdate;
+        uint256 firstEpochStart;
+        uint256 preLockRedemptionPeriod;
+    }
+
+    
